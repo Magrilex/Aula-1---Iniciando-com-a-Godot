60327342045d9ca06fa70c7fc13f6439c2dfe2b5
Pedro <pedro.anccilotto@gmail.com>
2026-05-29 08:02:03 -0300

    mapa feito

--- pikis-games/scripts/character_body_2d.gd
+++ pikis-games/scripts/character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 350.0
+const JUMP_VELOCITY = -600.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
